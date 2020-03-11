cbf75e52261ff7ec3e65b3617395db553a90d7b0
costavanelsas <costavanelsas@gmail.com>
2020-03-11 15:17:32 +0100

    FAQ start

---
+++ TripleOnboarding/FAQViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FAQViewController.swift
+//  TripleOnboarding
+//
+//  Created by Costa on 11/03/2020.
+//  Copyright © 2020 Triple. All rights reserved.
+//
+
+import UIKit
+
+class FAQViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
