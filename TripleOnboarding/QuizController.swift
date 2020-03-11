7c4df88a52fb4423a48dbab59cff59cc30b229b0
Youri Berentsen <Youriberen@gmail.com>
2020-03-11 11:50:21 +0100

    Quiz

---
+++ TripleOnboarding/QuizController.swift
@@ -0,0 +1,30 @@
+//
+//  QuizController.swift
+//  TripleOnboarding
+//
+//  Created by Youri Berentsen on 10/03/2020.
+//  Copyright © 2020 Triple. All rights reserved.
+//
+
+import UIKit
+
+class QuizController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
