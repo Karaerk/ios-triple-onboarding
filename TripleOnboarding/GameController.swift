e19f9dbda6030e130e075f9cf3cb8e48be1bc3a7
costavanelsas <costavanelsas@gmail.com>
2020-03-18 10:44:23 +0100

    werkt niet
    mist facts

---
+++ TripleOnboarding/GameController.swift
@@ -0,0 +1,18 @@
+//
+//  GameController.swift
+//  TripleOnboarding
+//
+//  Created by Youri Berentsen on 10/03/2020.
+//  Copyright © 2020 Triple. All rights reserved.
+//
+
+import UIKit
+
+class GameController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
